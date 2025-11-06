5eff26ba2f9e978451c731e3ccb75cc708d589a2
sudoabhi <abhinav004@outlook.in>
2025-11-06 22:42:57 +0530

    users model.py

--- craftersworld/users/serializers.py
+++ craftersworld/users/serializers.py
@@ -1,17 +1,71 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from .models import Organisation, User
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+
+# -----------------------------
+# Base Organisation Serializer
+# -----------------------------
+class OrganisationBaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organisation
+        fields = ["id", "name", "type"]
+
+
+class OrganisationDetailSerializer(OrganisationBaseSerializer):
+    class Meta(OrganisationBaseSerializer.Meta):
+        fields = OrganisationBaseSerializer.Meta.fields + [
+            "address",
+            "contact_email",
+            "contact_phone",
+            "created_at",
+        ]
+
+
+# -----------------------------
+# Base User Serializer
+# -----------------------------
+class UserBaseSerializer(serializers.ModelSerializer):
+    organisation = OrganisationBaseSerializer(read_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ["id", "username", "email", "role", "organisation"]
+
+
+class UserDetailSerializer(UserBaseSerializer):
+    organisation = OrganisationDetailSerializer(read_only=True)
+
+    class Meta(UserBaseSerializer.Meta):
+        fields = UserBaseSerializer.Meta.fields + [
+            "first_name",
+            "last_name",
+            "is_verified",
+            "profile_picture",
+        ]
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    organisation_id = serializers.PrimaryKeyRelatedField(
+        queryset=Organisation.objects.all(),
+        source="organisation",
+        write_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "organisation_id",
+            "role",
+        ]
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)  # hash password
+        user.save()
         return user
